7d83e3ee1ed3119c49855bd5a3632817384340cb
codeslw <sanvarkhujaev@gmail.com>
2023-06-06 09:00:10 +0500

    fixing 90

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+        <title>Внедрение, разработка и обучение систем менеджмента ISO</title>
+        <meta name="description" content="Внедрим и разработаем систему менеджмента на вашем предприятии. Подготовим и сопроводим вас ко внешним проверкам." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
       <body>
         <Main />
         <NextScript />
